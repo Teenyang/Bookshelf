bf7972529877b80d1bb64cff87620fff1271b35a
Teen Yang <chopcostop@gmail.com>
2021-09-12 19:33:28 +0800

    import bootstrapVue individual

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,17 @@
 import Vue from "vue";
 
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+// import BootstrapVue from "bootstrap-vue";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
+// Vue.use(BootstrapVue);
+
+import "@/assets/scss/_bootstrapVue.scss";
+import { NavbarPlugin } from "bootstrap-vue";
+Vue.use(NavbarPlugin);
+
+import { FormInputPlugin } from "bootstrap-vue";
+Vue.use(FormInputPlugin);
+
+import { BButton } from "bootstrap-vue";
+Vue.component("b-button", BButton);
