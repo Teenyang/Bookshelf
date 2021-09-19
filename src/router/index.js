7867e265f32f2d579731910e77a20f5ce17e9a00
Teen Yang <chopcostop@gmail.com>
2021-09-19 23:24:44 +0800

    add nav-item components for routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+import All from "@/views/All.vue";
+import Discount30 from "@/views/Discount30.vue";
+import Discount50 from "@/views/Discount50.vue";
+import Ithelp from "@/views/Ithelp.vue";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -18,6 +23,26 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/all",
+    name: "All",
+    component: All,
+  },
+  {
+    path: "/discount-30",
+    name: "Discount30",
+    component: Discount30,
+  },
+  {
+    path: "/discount-50",
+    name: "Discount50",
+    component: Discount50,
+  },
+  {
+    path: "/ithelp",
+    name: "Ithelp",
+    component: Ithelp,
   },
 ];
 
