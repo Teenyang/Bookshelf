aaf1a9730bdbbc3473a84f7b2b4af65d317723d4
Teen Yang <chopcostop@gmail.com>
2021-09-26 17:24:30 +0800

    fetch API with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { GET } from "@/utilities/API";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    bookList: {},
+  },
+  getters: {
+    bookList: (state) => state.bookList.list,
+  },
+  mutations: {
+    bookList(state, books) {
+      state.bookList = books;
+    },
+  },
+  actions: {
+    async fetchBookList(context) {
+      const books = await GET();
+      // console.log(books);
+      context.commit("bookList", books);
+    },
+  },
   modules: {},
 });
