eee50c6431d06cc1ca2ded63b8ea9cec3a236163
Teen Yang <chopcostop@gmail.com>
2022-01-24 20:50:47 +0800

    remove useless comment

--- src/store/index.js
+++ src/store/index.js
@@ -12,19 +12,8 @@ export default new Vuex.Store({
   },
   getters: {
     allBooks: (state) => state.bookList.list,
-    discountBooks: (state, getters) => (min, max) =>
-      getters.allBooks.filter(
-        (book) =>
-          min <= book.sellPrice / book.originPrice &&
-          book.sellPrice / book.originPrice < max
-      ),
-
     ithelpBook: (state, getters) =>
       getters["allBooks"].filter((book) => book.name.includes("鐵人賽")),
-
-    // ithelpBook: (state) =>
-    //   state.bookList.list.filter((book) => book.name.includes("鐵人賽")),
-
     trackingList: (state) => state.trackingList,
   },
   mutations: {
